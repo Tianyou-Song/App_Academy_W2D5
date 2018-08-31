c8f78596e367f5096e5bb9dbaf5afee024f0a1ce
Tianyou_Song <tim.d.song@gmail.com>
2018-08-31 16:17:29 -0700

    Implement HashMap class

--- hash_map_lru_cache/lib/p05_hash_map.rb
+++ hash_map_lru_cache/lib/p05_hash_map.rb
@@ -1,5 +1,54 @@
 require_relative 'p04_linked_list'
 
+class HashSet
+  include Enumerable
+  
+  attr_reader :count
+
+  def initialize(num_buckets = 8)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(key)
+    unless include?(key)
+      resize! if @count >= num_buckets
+      self[key] << key
+      @count += 1
+    end
+  end
+
+  def include?(key)
+    self[key].include?(key)
+  end
+
+  def remove(key)
+    if include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
+  end
+
+  private
+
+  def [](key)
+    @store[key.hash % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    original = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    original.flatten.each do |el|
+      insert(el)
+    end
+  end
+end
+
 class HashMap
   attr_reader :count
 
@@ -9,27 +58,45 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if self.include?(key)
+      bucket(key).update(key, val)
+    else
+      resize! if @count + 1 > num_buckets  
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    if self.include?(key)
+      bucket(key).remove(key)
+      @count -= 1
+    end 
   end
 
-  def each
+  def each(&prc)
+    @store.each do |bucket|
+      bucket.each do |node|
+        prc.call(node.key, node.val)
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -41,9 +108,18 @@ class HashMap
   end
 
   def resize!
+    original = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    original.each do |ll|
+      ll.each do |node|
+        self.set(node.key, node.val)
+      end
+    end
   end
 
   def bucket(key)
+    @store[key.hash % num_buckets]
     # optional but useful; return the bucket corresponding to `key`
   end
 end
